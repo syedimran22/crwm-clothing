80e22daa996b465619e3f7b4032e6f5979fe60bf
Syed Imran <41104393+syedimran22@users.noreply.github.com>
2022-03-06 02:05:53 +0530

    Added Routing method for initial pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Routes,Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes> 
     </div>
   );
 }
