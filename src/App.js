87fe24788fc83722a45ec6a753ecbe73ead7ac39
Syed Imran <41104393+syedimran22@users.noreply.github.com>
2022-03-22 20:55:05 +0530

    Implemented Firebase utils,including ability to store auth users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
-
+import { auth ,createUserProfileDocument} from './firebase/firebase.utils';
+import { onSnapshot } from 'firebase/firestore';
 class App extends React.Component {
 
   constructor(){
@@ -19,8 +19,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({currentUser:user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+          const userRef = await createUserProfileDocument(userAuth);
+          onSnapshot(userRef, (snapshot) =>{
+            this.setState(
+              {
+                currentUser : {
+                  id : snapshot.id,
+                  ...snapshot.data()
+                }
+              });
+
+              console.log(this.state);
+          });
+          
+      }
+      this.setState({currentUser:userAuth});
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getFirestore,setDoc,doc,getDoc } from 'firebase/firestore';
 
 const config = {
     apiKey: "AIzaSyDgW9e8WUJvXoAHXeGVUg11Bg_FeHxavgo",
@@ -11,9 +12,34 @@ const config = {
     measurementId: "G-SDSEJVBL8H"
   }; 
 
+export const createUserProfileDocument = async (userAuth,additionalData) => {
+    
+  if(!userAuth) return;
+  const userRef = doc(db,`users/${userAuth.uid}`);
+  const userSnap = await getDoc(userRef);
+ 
+   if(!userSnap.exists()){
+      const {displayName,email} = userAuth;
+      const createdAt = new Date();
+
+      try{
+      await setDoc(userRef,{
+        displayName : displayName,
+        email: email,
+        createdAt : createdAt,
+        ...additionalData
+        });
+
+      } catch(error){
+        console.log('error createing user',error.message);
+      }
+    }
+   return userRef;
+}
 
  const app = initializeApp(config);
  export const auth = getAuth(app);
+ export const db = getFirestore();
 
  const provider = new GoogleAuthProvider();
  provider.setCustomParameters({ prompt: 'select_account' });
