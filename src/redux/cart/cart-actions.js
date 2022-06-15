4622c59f17748e927513387dce902561b362c594
Syed Imran <41104393+syedimran22@users.noreply.github.com>
2022-06-15 23:22:42 +0530

    Added checkout page remove item and increase/decrease item actions

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -8,3 +8,14 @@ export const toggleCartHidden = user =>({
      type : CartActionTypes.ADD_ITEM,
      payload : item
  });
+
+ export const removeItem = item => ({
+    type : CartActionTypes.REMOVE_ITEM,
+    payload : item
+ });
+
+ export const clearItemFromCart = item => ({
+    type : CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+ });
+ 
